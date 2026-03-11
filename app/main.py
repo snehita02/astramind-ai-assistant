e636549385ecd8bedd53b89c64bf49d2052f86e1
Snehita Bharata <snehita.bharata@gmail.com>
2026-03-10 20:44:42 -0500

    Enable CORS for frontend

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="AstraMind Enterprise AI Platform",
     description="Multi-source, department-aware, enterprise RAG system with guardrails and production hardening.",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
